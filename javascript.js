c3bf9367f247e8b3bea8b84e31f1eadc175d39a4
Anna Orlova <orlovanna412@gmail.com>
2024-01-08 21:51:16 +0000

    add user choice and make one round of the game

--- javascript.js
+++ javascript.js
@@ -8,3 +8,39 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+let computerSelection = getComputerChoice();
+
+let userPrompt = prompt("Let's play Rock Paper Scissors game! Please write down your choice for this round.");
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let winMessage = "You win! " + playerSelection + " beats " + computerSelection;
+    let loseMessage = "You lose! " + computerSelection + " beats " + playerSelection;
+    if (playerSelection == computerSelection) {
+        let userPrompt = prompt("That was a tie! Let's replay the round");
+        return playRound(userPrompt, getComputerChoice());
+    } else if (playerSelection == "Rock") {
+        if (computerSelection == "Paper") {
+            return loseMessage;
+        } else if (computerSelection == "Scissors") {
+            return winMessage;
+        }
+    } else if (playerSelection == "Paper") {
+        if (computerSelection == "Scissors") {
+            return loseMessage;
+        } else if (computerSelection == "Rock") {
+            return winMessage;
+        }
+    } else if (playerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            return loseMessage;
+        } else if (computerSelection == "Paper") {
+            return winMessage;
+        }
+    } else {
+        return "Something went wrong, please rewrite the prompt";
+    }
+}
+
+console.log(playRound(userPrompt, computerSelection));
