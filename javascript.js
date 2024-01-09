157afe3ef5138072a8487f7bf05519a524c8d4b7
Anna Orlova <orlovanna412@gmail.com>
2024-01-09 03:03:08 +0000

    make counter of lose/win and report game result

--- javascript.js
+++ javascript.js
@@ -9,6 +9,9 @@ function getComputerChoice() {
     }
 }
 
+let computerCount = 0;
+let userCount = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     let winMessage = "You win! " + playerSelection + " beats " + computerSelection;
@@ -18,24 +21,31 @@ function playRound(playerSelection, computerSelection) {
         return playRound(userPrompt, getComputerChoice());
     } else if (playerSelection == "Rock") {
         if (computerSelection == "Paper") {
+            ++computerCount;
             return loseMessage;
         } else if (computerSelection == "Scissors") {
+            ++userCount;
             return winMessage;
         }
     } else if (playerSelection == "Paper") {
         if (computerSelection == "Scissors") {
+            ++computerCount;
             return loseMessage;
         } else if (computerSelection == "Rock") {
+            ++userCount;
             return winMessage;
         }
     } else if (playerSelection == "Scissors") {
         if (computerSelection == "Rock") {
+            ++computerCount;
             return loseMessage;
         } else if (computerSelection == "Paper") {
+            ++userCount;
             return winMessage;
         }
     } else {
-        return "Something went wrong, please rewrite the prompt";
+        let userPrompt = prompt("Something went wrong, please rewrite the prompt");
+        return playRound(userPrompt, getComputerChoice());
     }
 }
 
@@ -44,6 +54,11 @@ function game() {
         let computerSelection = getComputerChoice();
         let userPrompt = prompt("Let's play Rock Paper Scissors game! Please write down your choice for this round.");
         console.log(playRound(userPrompt, computerSelection));
+    }
+    if (userCount > computerCount) {
+        console.log("You are the winner! You won " + userCount + " times out of 5.");
+    } else {
+        console.log("Computer is the winner! You lost " + computerCount + " times out of 5.");
     }
 }
 
