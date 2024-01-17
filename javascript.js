9dc3adec53588e1a96e34b5a640e7b230ee5199d
Anna Orlova <orlovanna412@gmail.com>
2024-01-17 22:38:11 +0000

    remove 5 rounds logic, add buttons instead of prompts

--- javascript.js
+++ javascript.js
@@ -9,16 +9,29 @@ function getComputerChoice() {
     }
 }
 
+function getPlayerChoice(e) {
+    const btnClickedId = e.target.id;
+    if (btnClickedId == "paperBtn") {
+        return "Paper";
+    } else if (btnClickedId == "scissorsBtn") {
+        return "Scissors";
+    } else if (btnClickedId == "rockBtn") {
+        return "Rock";
+    }
+}
+
 let computerCount = 0;
 let userCount = 0;
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    //playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    //playerSelection = getPlayerChoice(e);
+
     let winMessage = "You win! " + playerSelection + " beats " + computerSelection;
     let loseMessage = "You lose! " + computerSelection + " beats " + playerSelection;
     if (playerSelection == computerSelection) {
-        let userPrompt = prompt("That was a tie! Let's replay the round");
-        return playRound(userPrompt, getComputerChoice());
+        //let userPrompt = prompt("That was a tie! Let's replay the round");
+        return "That was a tie! Let's replay the round";
     } else if (playerSelection == "Rock") {
         if (computerSelection == "Paper") {
             ++computerCount;
@@ -44,11 +57,20 @@ function playRound(playerSelection, computerSelection) {
             return winMessage;
         }
     } else {
-        let userPrompt = prompt("Something went wrong, please rewrite the prompt");
-        return playRound(userPrompt, getComputerChoice());
+        //let userPrompt = prompt("Something went wrong, please rewrite the prompt");
+        return "Something went wrong, please rewrite the prompt";
     }
 }
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(playRound(getPlayerChoice(e), getComputerChoice()));
+  });
+});
+
+/*
 function game() {
     for (let i = 0; i < 5; i++) {
         let computerSelection = getComputerChoice();
@@ -63,3 +85,4 @@ function game() {
 }
 
 game();
+*/
